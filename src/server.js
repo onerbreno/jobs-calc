dbad6c9a4ef39b9e0123c1128c4350e979bb1d18
breno <onerbreno@outlook.com>
2023-02-05 13:44:03 -0300

    feat adicionado template view em ejs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,13 @@
 const express = require('express')
-
 const server = express();
+const routes = require("./routes")
 
+// usando  template engine
+server.set('view engine', 'ejs')
+
+// habilitar arquivos statics
+server.use(express.static("public"))
+
+// routes
+server.use(routes)
 server.listen(3000, () => console.log("Rodando na porta 5500"))
