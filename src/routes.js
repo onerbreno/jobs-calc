591a1d440200db8c34528b552d4e20ab4ac9bfa5
Breno <onerbreno@outlook.com>
2023-02-07 21:20:02 -0300

    feat envia jobs com valores cálculados para página principal

--- src/routes.js
+++ src/routes.js
@@ -13,8 +13,53 @@ const profile = {
     "value-hour": 75
 }
 
+const jobs = [
+    {
+        id: 1,
+        name: "Pizzaria Guloso",
+        "daily-hours": 2,
+        "total-hours": 60,
+        createdAt: Date.now()
+    },
+    {
+        id: 2,
+        name: "OneTwo Project",
+        "daily-hours": 2,
+        "total-hours": 47,
+        createdAt: Date.now()
+    }
+]
+
+function remainingDays(job) {
+    const estimatedDays = (job["total-hours"] / job["daily-hours"]).toFixed()
+
+    const createdDate = new Date(job.createdAt)
+    const dueDay = createdDate.getDate() + Number(estimatedDays)
+    const dueDateInMs = createdDate.setDate(dueDay)
+
+    const timeDiffInMs = dueDateInMs - Date.now()
+    const dayInMs = 1000 * 60 * 60 * 24
+    const dayDiff = Math.floor(timeDiffInMs / dayInMs)
+    return dayDiff
+}
 
 
+routes.get('/', (req, res) => {
+    const updatedJobs = jobs.map((job) => {
+        
+        const remaining = remainingDays(job)
+        const status = remaining <= 0 ? 'done' : 'progress'
+
+        return {
+            ...job,
+            remaining,
+            status,
+            budget: profile["value-hour"] * job["total-hours"]
+        }
+    })
+    
+    return res.render(views + 'index', { jobs: updatedJobs })
+})
 
 routes.get('/job', (req, res) => res.render(views + 'job'))
 routes.post('/job', (req, res) => {
